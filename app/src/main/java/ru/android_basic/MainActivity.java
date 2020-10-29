c05a5856cecbacf28ddcb8470cce179b37648e79
Arsen Saliev <arsensaliev03@gmail.com>
2020-10-29 17:39:41 +0600

    feat: finished homework

--- app/src/main/java/ru/android_basic/MainActivity.java
+++ app/src/main/java/ru/android_basic/MainActivity.java
@@ -3,12 +3,36 @@ package ru.android_basic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinnerLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerLocation = findViewById(R.id.spinnerLocation);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.locations,R.layout.color_spinner_layout);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
+        spinnerLocation.setAdapter(adapter);
+        spinnerLocation.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(getApplicationContext(), adapterView.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
