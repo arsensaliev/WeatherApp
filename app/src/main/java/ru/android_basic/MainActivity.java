743c92c32eeeb15a958d0254e629a3123b362581
Arsen Saliev <arsensaliev03@gmail.com>
2020-11-05 17:07:37 +0600

    feat: add homework 3

--- app/src/main/java/ru/android_basic/MainActivity.java
+++ app/src/main/java/ru/android_basic/MainActivity.java
@@ -2,37 +2,119 @@ package ru.android_basic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner spinnerLocation;
+    private TextView cityTemperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinnerLocation = findViewById(R.id.spinnerLocation);
+        String instanceState;
+        if (savedInstanceState == null) {
+            instanceState = "Первый запуск!";
+            Log.d("MainActivity", "Первый запуск");
+        } else {
+            instanceState = "Повторный запуск!";
+            Log.d("MainActivity", "Повторный запуск");
+        }
+        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.locations,R.layout.color_spinner_layout);
+        spinnerLocation = findViewById(R.id.spinnerLocation);
+        cityTemperature = findViewById(R.id.city_temperature);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.locations, R.layout.color_spinner_layout);
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
         spinnerLocation.setAdapter(adapter);
         spinnerLocation.setOnItemSelectedListener(this);
     }
 
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        Toast.makeText(getApplicationContext(), adapterView.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+        StringBuilder city = new StringBuilder(adapterView.getSelectedItem().toString());
+        Log.d("MainActivity", String.valueOf(city));
+        if (String.valueOf(city).equals("Москва")) {
+            cityTemperature.setText("+12");
+        } else if (String.valueOf(city).equals("Алматы")) {
+            cityTemperature.setText("+20");
+        } else if (String.valueOf(city).equals("Лондон")) {
+            cityTemperature.setText("+9");
+        } else if (String.valueOf(city).equals("Париж")) {
+            cityTemperature.setText("+18");
+        }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
-
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onDestroy");
+    }
+
 }
